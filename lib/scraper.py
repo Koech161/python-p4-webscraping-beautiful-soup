68c277b2e9ea4cf93a1f94e9754e1804e4571ea5
Abel Koech <koechabel161@gmail.com>
2024-10-06 11:45:04 +0300

    webscrapping

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,13 @@
-from turtle import ht
+# from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+# print(doc.select('.column-highlights.pt-sm.pb-sm.bg-white'))
+courses = doc.select('.column-highlights.pt-sm.pb-sm.bg-white')[0].attrs
+
+# for course in courses:
+print(courses)
